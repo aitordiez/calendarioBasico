9b11b5b0de35f3e5237f0bbbfa10d5a34c78f83c
Aitor Diez Fernandez <aitorchic@gmail.com>
2017-09-22 15:00:34 +0200

    (Creada la pagina de inicio

---
+++ Aitor/index.html
@@ -0,0 +1,8 @@
+<html>
+<head>
+  <title>DAM</title>
+</head>
+<body>
+<h1>Empezamos elcurso de DAM</h1>
+</body>
+</html>
---
+++ Desktop/DAM.java
@@ -0,0 +1,6 @@
+public class DAM{
+   public static void main(String args [ ] ){
+
+	System.out.println("Aitor Diez Fernandez");
+  }
+}
